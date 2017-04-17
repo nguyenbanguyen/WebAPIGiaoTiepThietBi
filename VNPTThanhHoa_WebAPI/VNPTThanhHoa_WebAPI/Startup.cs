e8007b4821d525dbaa2abe7031871c1c98a4e19c
nguyen ba nguyen <dns07062013@yahoo.com.vn>
2017-04-17 14:34:16 +0700

    Add Swagger API Helper Page

--- VNPTThanhHoa_WebAPI/VNPTThanhHoa_WebAPI/Startup.cs
+++ VNPTThanhHoa_WebAPI/VNPTThanhHoa_WebAPI/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace VNPTThanhHoa_WebAPI
 {
@@ -29,11 +30,37 @@ namespace VNPTThanhHoa_WebAPI
         {
             // Add framework services.
             services.AddMvc();
+            // config swagger helping page below
+
+            services.AddLogging();
+
+            // Add our repository type-- cần config
+            //services.AddSingleton<ITodoRepository, TodoRepository>();
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            //config title và version hiển thị tại trang /swagger 
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "VNPTThanhHoaWebAPI", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            // config swagger helping page below
+            app.UseMvcWithDefaultRoute();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS etc.), specifying the Swagger JSON endpoint.
+            // Link của Json swagger sử dụng để tạo API Helper Page
+            app.UseSwaggerUI(c =>
+            {
+                // URL của json và Description
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
